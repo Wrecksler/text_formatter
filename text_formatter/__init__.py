fee2a218875c55782fefb5af7a4188142daedbf8
Wrecksler <wrecksler@gmail.com>
2025-03-15 21:37:14 +0300

    improve headings

--- text_formatter/__init__.py
+++ text_formatter/__init__.py
@@ -40,6 +40,11 @@ def collapse(name, collapse_content, options, parent, context):
 {collapse_content}
 </details>"""
 
+def heading(name, content, options, parent, context):
+    level = options.get("level", "4")
+
+    return f"""<h{int(level)}>{content}</h{int(level)}>"""
+
 def line_all_equal(s, min_length=3):
     if len(s) < min_length:
         return False
@@ -75,8 +80,8 @@ bbcode_parser.add_simple_formatter(
 )
 bbcode_parser.add_formatter("icon", icon)
 bbcode_parser.add_simple_formatter("code", "[code]\n%(value)s\n[/code]")
-bbcode_parser.add_simple_formatter("heading", "<h1>%(value)s</h1>")
 bbcode_parser.add_formatter("collapse", collapse)
+bbcode_parser.add_formatter("heading", heading)
 
 
 def html_to_bbcode(html):
@@ -238,6 +243,12 @@ also some [b]text[/b] on another line
 
 [collapse=Collapsible title]Some collapsible content[/collapse]
 
-[collapse]Some collapsible content without title[/collapse]"""
+[collapse]Some collapsible content without title[/collapse]
+
+
+[collapse=Single quotes in arguments ' will break the parser here sadly]
+Some content
+[/collapse]
+"""
 
     TextFormatter.from_bbcode(example_bbcode).print_demo()
